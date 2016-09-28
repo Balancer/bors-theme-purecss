0342e53d92de2ca5d7a65e4d723417eaf7df7006
Balancer <balancer@balancer.ru>
2016-09-28 18:33:40 +0300

    ++

---
+++ src/Layout/PureCss.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace B2\Layout;
+
+class PureCss extends \B2\Layout
+{
+	function table_class() { return 'pure-table pure-table-bordered'; }
+	function ul_tab_class() { return ''; }
+}
---
+++ src/Layout/PureCss/breadcrumbs.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+$result = [];
+
+foreach(bors_lib_object::parent_lines($self) as $breadcrumbs_line)
+{
+	$line = [];
+	foreach($breadcrumbs_line as $x)
+	{
+		if(empty($x['is_active']))
+			$line[] = "<a href=\"{$x['url']}\">{$x['title']}</a>";
+		else
+			$line[] = "<a href=\"{$x['url']}\"><b>{$x['title']}</b></a>";
+	}
+
+	$result[] = join(' » ', $line);
+}
+
+return join("<br/>\n", $result);
